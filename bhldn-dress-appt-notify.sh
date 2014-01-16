56a73479c230da53280bda75703c4148f77b6694
alec ditonto <alec.ditonto@gmail.com>
2014-01-16 01:12:10 -0800

    organized files into a tmp folder, and added a flag to remove the necessity to have two separates scripts for email repeats -- now there is a flag in the script

--- bhldn-dress-appt-notify.sh
+++ bhldn-dress-appt-notify.sh
@@ -4,14 +4,20 @@
 # days: 2225 | 2226
 # time: >=50400 | <=54000
 
-clear
+clear # clean things up a bit
 
 # this is the URL that will be tested against
 url="http://bhldnbeverlyhills.fullslate.com/services/1?start=2218&view=bare"
 # this is the term that will be searched for
 searchterm="/book?day=2223&services%5B%5D=1&time=50400&view=bare"
+# email that notification is sent to
+email="alec.ditonto@gmail.com"
+# flag file -- used to determine if the script ever ran successfully
+flag="tmp/flag.txt"
+# var used to determine if this should repeatedly send emails
+repeat=true
 
-if [ ! -f found.txt ]
+if [ ! -f $flag ] || [ $repeat ]
   then
     
     echo "Retrieving web data from: http://bhldnbeverlyhills.fullslate.com/services/1?start=2218&view=bare ..."
@@ -20,12 +26,12 @@ if [ ! -f found.txt ]
     content="$(curl -s "$url")"
 
     # put content into text.txt file
-    echo "$content"> text.txt
+    echo "$content"> tmp/text.txt
 
     echo "Analyzing results..."
 
     #search for the search term
-    result=$(grep $searchterm text.txt)
+    result=$(grep $searchterm tmp/text.txt)
 
     #check status of the grep command
     if [ $? -eq 0 ]
@@ -33,14 +39,14 @@ if [ ! -f found.txt ]
         echo "Date found..."
 
         #create the found doc so the script won't repeat itself  
-        echo "found"> found.txt
+        echo "flagged"> $flag
         
         #create a file to hold the email message and create the message
-        message="message.txt"
+        message="tmp/message.txt"
         echo "Date found at http://bhldnbeverlyhills.fullslate.com/services/1?start=2218&view=bare"> $message 
         
         #send the email
-        $(mail -s "date found" "hiptonto@50cubes.com" < $message)
+        $(mail -s "date found" alec.ditonto@gmail.com < $message)
         
         echo "Email sent..."
       else
