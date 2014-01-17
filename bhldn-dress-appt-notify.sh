e921b2ef9d4b2660dc3d0b85a2916466a4a865a3
Alec Ditonto <hiptonto@50cubes.com>
2014-01-16 19:44:30 -0800

    update to sort through an array -- now just need to get it running on a cron with a computer that can send email successfully!

--- bhldn-dress-appt-notify.sh
+++ bhldn-dress-appt-notify.sh
@@ -2,14 +2,13 @@
 # this script hits this URL http://bhldnbeverlyhills.fullslate.com/services/1?start=2218&view=bare
 # every minute to look for an opening on 2/8 or 2/9
 # days: 2225 | 2226
-# time: >=50400 | <=54000
 
 clear # clean things up a bit
 
 # this is the URL that will be tested against
 url="http://bhldnbeverlyhills.fullslate.com/services/1?start=2218&view=bare"
 # this is the term that will be searched for
-searchterm="/book?day=2223&services%5B%5D=1&time=50400&view=bare"
+searchterms=( "/book?day=2225" "/book?day=2226" )
 # email that notification is sent to
 email="alec.ditonto@gmail.com"
 # flag file -- used to determine if the script ever ran successfully
@@ -30,29 +29,32 @@ if [ ! -f $flag ] || [ $repeat ]
 
     echo "Analyzing results..."
 
-    #search for the search term
-    result=$(grep $searchterm tmp/text.txt)
+    for i in "${searchterms[@]}"
+    do
+        echo $i
+        #search for the search term
+        result=$(grep $i tmp/text.txt)
 
-    #check status of the grep command
-    if [ $? -eq 0 ]
-      then
-        echo "Date found..."
+        #check status of the grep command
+        if [ $? -eq 0 ]
+          then
+            echo "Date found..."
 
-        #create the found doc so the script won't repeat itself  
-        echo "flagged"> $flag
-        
-        #create a file to hold the email message and create the message
-        message="tmp/message.txt"
-        echo "Date found at http://bhldnbeverlyhills.fullslate.com/services/1?start=2218&view=bare"> $message 
-        
-        #send the email
-        #$(mail -s "date found" "alec.ditonto@gmail.com "< $message)
-        
-        echo "Email sent..."
-      else
-        echo 'No date found...'
-    fi
+            #create the found doc so the script won't repeat itself  
+            echo "flagged"> $flag
+            echo $i
+            #create a file to hold the email message and create the message
+            message="tmp/message.txt"
+            echo "Date found at http://bhldnbeverlyhills.fullslate.com/services/1?start=2218&view=bare" "$i"> $message 
+            
+            #send the email
+            $(mail -s "date found" $email< $message)
+            
+            echo "Email sent..."
+          else
+            echo 'No date found...'
+        fi
+    done
   else
     echo "File found. Terminating..."
 fi
-
